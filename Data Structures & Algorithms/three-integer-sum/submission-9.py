0af7c9ccacd6b7315790eb6870f2c86fe3b78703
Brent <166071737+JussBrent@users.noreply.github.com>
2026-09-09 00:59:03 -0400

    Add: three-integer-sum - submission-9

---
+++ Data Structures & Algorithms/three-integer-sum/submission-9.py
@@ -0,0 +1,34 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        #have an index running through then your lo and high
+        #sort it out first
+        nums.sort()
+        res = []
+        # -4 -1 -1 0 1 2
+        for i in range(len(nums)):
+            
+            lo = i + 1
+            high = len(nums) - 1
+
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            while lo < high:
+                if nums[i] + nums[lo] + nums[high] > 0:
+                    high -= 1
+                elif nums[i] + nums[lo] + nums[high] < 0:
+                    lo += 1
+                elif nums[i] + nums[lo] + nums[high] == 0:
+                    res.append([nums[i], nums[lo], nums[high]])
+                    while lo < high and nums[lo] == nums[lo + 1]:
+                        lo += 1
+                    while lo < high and nums[high] == nums[high - 1]:
+                        high -= 1
+                    lo += 1
+                    high -= 1
+                    
+        return res
+                
+
+        
